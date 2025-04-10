62a642452a7a3067fae2f36d6e63a27962ef34ed
bhavikshangari <bhavikshangari@gmail.com>
2025-04-10 18:02:55 +0530

    added rfl-self client and aggregator

---
+++ clients/rfl_self.py
@@ -0,0 +1,181 @@
+import numpy as np
+import random
+import torch
+import torchvision
+import torch.nn as nn
+import json 
+from abc import ABC
+from typing import Tuple, Union
+import copy
+from torch.utils.data import DataLoader, Subset, Dataset, random_split
+from torch.utils.tensorboard import SummaryWriter
+import torch.nn as nn
+from torch.optim import Adam, SGD
+from tqdm import tqdm
+import os
+from utils.generic import load_json
+from clients.base import BaseClient
+
+
+class RFL_SelfClient(BaseClient):
+
+    def __init__(self, name, id, dataset, device, model, ratio, shuffle, train_config, is_selfish:bool = False, selfishness:int=0):
+        super().__init__(name, id, dataset, device, model, ratio, shuffle, train_config)
+        self.progress_bar = tqdm(range(self.gepochs*self.lepochs), leave=False)
+        self.is_selfish = is_selfish
+        if self.is_selfish:
+            self.previous_model = self.model
+            cwd = os.getcwd()
+            main_cfg_path = f'{cwd}/configs/experiment_config.json'
+            main_cfg = load_json(main_cfg_path)
+            self.k = main_cfg.get("clients", 5)
+            self.delta_hat_s = None
+            self.selfishness = selfishness
+            print('Number of Clients: ', self.k)
+            
+
+
+    
+    def _split_data(self):
+        train_size = int(self.ratio* len(self.dataset))
+        test_size = len(self.dataset) - train_size
+        self.train_data, self.test_data = random_split(dataset=self.dataset, lengths=[train_size, test_size])
+        
+        while self.batch_size > 1 and train_size // self.batch_size == 0: self.batch_size //= 2 
+
+        train_loader = DataLoader(dataset=self.train_data,
+                                  batch_size=self.batch_size,
+                                  shuffle=self.shuffle,
+                                  drop_last=True 
+                                )
+        test_loader = DataLoader(
+                                dataset=self.test_data,
+                                batch_size=self.batch_size,
+                                shuffle=self.shuffle
+        )
+
+        return train_loader, test_loader
+    
+    def _parse_train_config(self):
+        train_config = load_json(self.train_config)
+
+
+        self.learning_rate = train_config.get('learning_rate', 0.01) 
+        optimizer_name = train_config.get('optimizer', 'Adam')  
+        self.batch_size = train_config.get('batch_size', 64)  
+        self.optimizer_params = train_config.get('optimizer_params', {})
+        self.gepochs = train_config.get('num_global_epochs', 10)
+        self.lepochs = train_config.get('num_local_epochs', 20)
+        loss_name = train_config.get('loss', 'CE')
+        self.local_epochs_completed = 0
+        self.global_epochs_completed = 0
+
+        
+        if optimizer_name.lower() == "adam" :
+            self.optimizer = Adam(self.model.parameters(),
+                                lr=self.learning_rate,
+                                betas=(self.optimizer_params.get('b1', 0.9),
+                                        self.optimizer_params.get('b2', 0.999)),
+                                eps=self.optimizer_params.get('eps', 1e-8),
+                                weight_decay=self.optimizer_params.get('weight_decay', 0),
+                                amsgrad=self.optimizer_params.get('amsgrad', False)
+                                )
+
+            
+        elif optimizer_name.lower() == "sgd" :
+            self.optimizer = SGD(self.model.parameters(),
+                            lr=self.learning_rate,
+                            momentum=self.optimizer_params.get('momentum', 0),
+                            dampening=self.optimizer_params.get('dampening', 0),
+                            weight_decay=self.optimizer_params.get('weight_decay', 0),
+                            nesterov=self.optimizer_params.get('nesterov', False)
+                            )
+            
+        else :
+            raise ValueError('Optimizer not implemented')
+        
+        if loss_name == 'CE' :
+            self.loss = nn.CrossEntropyLoss()
+        else :
+            raise ValueError('Loss not passed correctly')
+        
+
+
+        return 
+    
+    def local_train(self, num_epochs) -> None:
+        # progress_bar = tqdm(range(num_epochs), desc=f'{self.name} in its 0/{num_epochs} epoch in Global Epoch {self.global_epochs_completed}/{self.gepochs}')
+        torch.cuda.empty_cache()
+
+        for i in range(num_epochs):
+            self.progress_bar.set_description(f'{self.name} in its {self.local_epochs_completed + i+1}/{self.gepochs*self.lepochs} epoch in Global Epoch {self.global_epochs_completed}/{self.gepochs}')
+            self.model = self.model.to(self.device)
+
+            self.model.train()
+            train_loss, train_correct, train_total =  0, 0, 0
+
+
+            # training 
+            for inputs, labels in self.train_loader : 
+                inputs, labels = inputs.to(self.device), labels.to(self.device)
+                self.optimizer.zero_grad()
+                # print(inputs.device, next(self.model.parameters()).device)
+                outputs = self.model(inputs)
+                loss = self.loss(outputs, labels)
+                loss.backward()
+                self.optimizer.step()
+                
+                train_loss += loss.item()
+                _, predicted = outputs.max(1)
+                train_total += labels.shape[0]
+                train_correct += predicted.eq(labels).sum().item()
+                # print(train_correct, train_total, labels.shape)
+
+                
+            self.local_epochs_completed += 1
+            
+            #testing
+            test_loss, test_correct, test_total = 0, 0, 0
+            self.model.eval()
+            with torch.no_grad(): 
+                for inputs, labels in self.test_loader : 
+                    inputs, labels = inputs.to(self.device), labels.to(self.device)
+                    outputs = self.model(inputs)
+                    loss = self.loss(outputs, labels)
+
+                    test_loss += loss.item()
+                    _, predicted = outputs.max(1)
+                    test_total += labels.shape[0]
+                    test_correct += predicted.eq(labels).sum().item()
+                    torch.cuda.empty_cache()
+
+            train_accuracy = (train_correct/ train_total)*100
+            self.metrics['train_accuracies'] += [train_accuracy]
+            self.metrics['train_losses'] += [train_loss]
+
+            test_accuracy = (test_correct/test_total)*100
+            self.metrics['test_accuracies'] += [test_accuracy]
+            self.metrics['test_losses'] += [test_loss]
+        
+        self.global_epochs_completed += 1
+
+        torch.cuda.empty_cache()
+        
+        if self.is_selfish:
+            delta_k_without_s = []
+            if self.delta_hat_s is None:
+                self.delta_hat_s = []
+                for ((_, cur_weights), (_, prev_weights)) in zip(self.model.named_parameters(), self.previous_model.named_parameters()):
+                    delta_hat_t_k_minus_s = (self.k*(cur_weights - prev_weights)) / (self.k - 1)
+                    deltahat_s = self.selfishness * self.k *(cur_weights.grad - delta_hat_t_k_minus_s) + delta_hat_t_k_minus_s
+                    self.delta_hat_s.append(deltahat_s)
+                    cur_weights.grad = deltahat_s
+            else:
+                for i, ((_, cur_weights), (_, prev_weights)) in enumerate(zip(self.model.named_parameters(), self.previous_model.named_parameters())):
+                    delta_hat_t_k_minus_s = (self.k*(cur_weights - prev_weights) - self.delta) / (self.k - 1)
+                    deltahat_s = self.selfishness * self.k *(cur_weights.grad - delta_hat_t_k_minus_s) + delta_hat_t_k_minus_s
+                    self.delta_hat_s[i] = deltahat_s
+                    cur_weights.grad = deltahat_s
+            self.previous_model = copy.deepcopy(self.model)
+                            
+        return 
---
+++ main_rfl_self.py
@@ -0,0 +1,160 @@
+import numpy as np
+import pandas as pd
+import random
+import os
+import argparse
+
+import torch
+import torchvision
+
+from torchvision.datasets import CIFAR10, MNIST, CIFAR100
+from torchvision import transforms 
+from torch.utils.data import Dataset, Subset, DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+
+from utils.generic import load_json, dataset_normalization_values
+from datafactory.dirichilet_dist import distribute_data_dirichilet
+from datafactory.iid_dist import distribute_data_iid
+
+from models.resnet18 import Resnet18_model
+from models.cnn import CNN
+from clients.rfl_self import RFL_SelfClient
+from servers.rfl_self import RFL_SelfServer
+
+#paths
+cwd = os.getcwd()
+data_dir = f'{cwd}/data'
+main_cfg_path = f'{cwd}/configs/experiment_config.json'
+
+#made a cfg to be given to the server for better usage....as complexity grows, keep adding cfg paths here
+all_cfgs = {
+    'main' : main_cfg_path
+}
+
+
+
+main_cfg = load_json(main_cfg_path)
+# print(main_cfg)
+
+dataset_name = main_cfg.get("dataset", "cifar10")
+seed = main_cfg.get("seed", 69)
+distribution = main_cfg.get("dist", "iid")
+shuffle = main_cfg.get("shuffle", True)
+model_name = main_cfg.get("model", "resnet")
+device = main_cfg.get("device", "cuda")
+save = main_cfg.get("save", False)
+ratio = main_cfg.get("ratio", 0.9)
+training_config_file_path = main_cfg.get("training", "")
+num_clients = main_cfg.get("clients", 5)
+
+all_cfgs['train'] = training_config_file_path
+
+# exp_name = '-'.join([f'{key}-{value}' for key, value in vars(args).items()])
+
+torch.manual_seed(seed)
+np.random.seed(seed)
+
+
+# torch.manual_seed(seed)
+# np.random.seed(seed)
+
+client_id_name_map = {f'C{i}' : f'Client-{i}' for i in range(num_clients)} # this is redundant for now...will use it for better purposes later
+
+if dataset_name == 'cifar10' : 
+    dataset = torchvision.datasets.CIFAR10(
+        root=f'{data_dir}/CIFAR10',
+        train=True,
+        download=True,
+        transform=transforms.ToTensor())
+    
+elif dataset_name == 'mnist' :
+    dataset = torchvision.datasets.MNIST(
+        root=f'{data_dir}/MNIST',
+        train=True,
+        download=True,
+        transform=transforms.ToTensor())
+    
+elif dataset_name == 'cifar100' :
+    dataset = torchvision.datasets.CIFAR100(
+        root=f'{data_dir}/CIFAR100',
+        train=True,
+        download=True,
+        transform=transforms.ToTensor())    
+
+
+mean, std = dataset_normalization_values(dataset=dataset)
+
+
+dataset.transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=mean, std=std)
+])
+
+
+
+num_classes = len(dataset.classes)
+
+if distribution == 'dirichilet' :
+    
+    alphas = [0.1 for _ in range(num_clients)]  # change this for testing with various alphas
+    
+    datasets = distribute_data_dirichilet(
+        dataset=dataset, 
+        num_clients=num_clients, 
+        alphas=alphas, 
+        seed=seed,
+        shuffle=shuffle
+        )
+
+elif distribution == 'iid' : 
+    datasets = distribute_data_iid(
+        dataset=dataset,
+        num_clients=num_clients,
+        seed=seed,
+        shuffle=shuffle
+    )
+print('Data Distribution is done')
+
+
+
+models = {
+    i : CNN(in_channels=dataset.data.shape[-1], 
+                       num_classes=num_classes
+                       ) for i in range(num_clients)
+}
+
+print('Models Initialized')
+
+
+clients = {client_id : RFL_SelfClient(name=client_id_name_map[client_id],
+                                    id=client_id,
+                                    dataset=datasets[ind],
+                                    device=device,
+                                    model=models[ind],
+                                    ratio=ratio,
+                                    shuffle=shuffle,
+                                    train_config=training_config_file_path
+                                    ) for ind, client_id in enumerate(client_id_name_map.keys())
+           }
+
+
+print('Clients have been initalized')
+
+
+server = RFL_SelfServer(clients=clients,
+                      configs=all_cfgs,
+                      model_sample=list(models.values())[-1],
+                      data=None, 
+                      device=device
+                      )
+train_config = load_json(training_config_file_path)
+
+server.aggregate(
+    num_global_epochs=train_config.get('num_global_epochs', 10),
+    num_local_epochs=train_config.get('num_local_epochs', 20),
+    save=save,
+    save_step=1
+)
+
+
